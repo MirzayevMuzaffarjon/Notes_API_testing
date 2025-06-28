c2400f9a122849baff3c1d4607c410627e86eb0f
m_m_macbook <mirzayevmuzaffar525@gmail.com>
2025-06-28 20:58:35 +0500

    tests optimized

--- API_test/Notes_test.py
+++ API_test/Notes_test.py
@@ -22,6 +22,18 @@ def test_delete_home_note(notes_api, get_token):
     test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
     if test1 != 0: pytest.fail()
 
+def test_create_work_note(notes_api, get_token):
+    response = notes_api.call_create_notes_api(body=bodys.body_for_create_work_notes, headers=notes_api.get_default_header_with_auth(get_token))
+    global work_note_id
+    work_note_id = response.json()["data"]["id"]
+    test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
+    if test1 != 0: pytest.fail()
+
+def test_delete_work_note(notes_api, get_token):
+    response = notes_api.call_delete_notes_api(note_id=work_note_id, headers=notes_api.get_default_header_with_auth(get_token))
+    test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
+    if test1 != 0: pytest.fail()
+
 def test_user_delete(users_api, get_token):
     response = users_api.call_user_delete_api(headers=users_api.get_default_header_with_auth(get_token))
     test1 = users_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
