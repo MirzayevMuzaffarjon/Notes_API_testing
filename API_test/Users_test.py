0d7066e1379a0a76ff59ac3a18721d57432cedd7
m_m_macbook <mirzayevmuzaffar525@gmail.com>
2025-06-28 12:33:30 +0500

    tests optimized

--- API_test/Users_test.py
+++ API_test/Users_test.py
@@ -4,12 +4,16 @@ from API_test.test_data import bodys, headers
 def test_user_register(users_api):
     response = users_api.call_user_register_api(body=bodys.body_for_user_register, headers=headers.header_default)
     test1 = users_api.verify_status_code_is(expected_status_code=201, actual_status_code=response.status_code)
-    if test1 != 0: raise
+    test2 = users_api.verify_name_is_correct(response_body=response.json(), expected_name=users_api.name)
+    test3 = users_api.verify_email_is_correct(response_body=response.json(), expected_email=users_api.email)
+    if test1 + test2 + test3 != 0: raise
 
 def test_user_login(users_api):
     response = users_api.call_user_login_api(body=bodys.body_for_user_login, headers=headers.header_default)
     test1 = users_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
-    if test1 != 0: raise
+    test2 = users_api.verify_name_is_correct(response_body=response.json(), expected_name=users_api.name)
+    test3 = users_api.verify_email_is_correct(response_body=response.json(), expected_email=users_api.email)
+    if test1 + test2 + test3 != 0: raise
 
 def test_user_profile_get(users_api, get_token):
     response = users_api.call_user_profile_api(headers=users_api.get_default_header_with_auth(get_token))
@@ -19,10 +23,7 @@ def test_user_profile_get(users_api, get_token):
     if test1 + test2 + test3 != 0: raise
 
 def test_user_edit_profile(users_api, get_token):
-    response = users_api.call_edit_profile_api(
-        body=bodys.body_for_edit_profile,
-        headers=users_api.get_default_header_with_auth(get_token))
-
+    response = users_api.call_edit_profile_api(body=bodys.body_for_edit_profile, headers=users_api.get_default_header_with_auth(get_token))
     test1 = users_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
     test2 = users_api.verify_phone_number_is_correct(response_body=response.json(), expected_phone_number=users_api.phone)
     test3 = users_api.verify_company_name_is_correct(response_body=response.json(), expected_company_name=users_api.company)
