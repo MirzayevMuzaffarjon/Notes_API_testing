da6e420584d9ae6640bcb01af9b503e73eca3992
m_m_macbook <mirzayevmuzaffar525@gmail.com>
2025-06-30 15:29:08 +0500

    tests optimized

--- API_test/Notes_test.py
+++ API_test/Notes_test.py
@@ -14,14 +14,6 @@ def test_create_home_note(notes_api, get_token):
     test2 = notes_api.verify_status_massage(response_body=response.json())
     if test1 + test2 != 0: pytest.fail()
 
-def test_get_home_note(notes_api, get_token):
-    response = notes_api.call_get_notes_api(note_id=home_note_id, headers=notes_api.get_default_header_with_auth(get_token))
-    test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
-    test2 = notes_api.verify_note_title(expected_title=notes_api.home_note_title, response_body=response.json())
-    test3 = notes_api.verify_note_description(expected_description=notes_api.home_note_description, response_body=response.json())
-    test4 = notes_api.verify_note_category(expected_category=notes_api.home_note_category, response_body=response.json())
-    if test1 + test2 + test3 + test4 != 0: pytest.fail()
-
 def test_create_work_note(notes_api, get_token):
     response = notes_api.call_create_notes_api(body=bodys.body_for_create_work_notes, headers=notes_api.get_default_header_with_auth(get_token))
     global work_note_id
@@ -30,14 +22,6 @@ def test_create_work_note(notes_api, get_token):
     test2 = notes_api.verify_status_massage(response_body=response.json())
     if test1 + test2 != 0: pytest.fail()
 
-def test_get_work_note(notes_api, get_token):
-    response = notes_api.call_get_notes_api(note_id=work_note_id, headers=notes_api.get_default_header_with_auth(get_token))
-    test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
-    test2 = notes_api.verify_note_title(expected_title=notes_api.work_note_title, response_body=response.json())
-    test3 = notes_api.verify_note_description(expected_description=notes_api.work_note_description, response_body=response.json())
-    test4 = notes_api.verify_note_category(expected_category=notes_api.work_note_category, response_body=response.json())
-    if test1 + test2 + test3 + test4 != 0: pytest.fail()
-
 def test_create_personal_note(notes_api, get_token):
     response = notes_api.call_create_notes_api(body=bodys.body_for_create_personal_notes, headers=notes_api.get_default_header_with_auth(get_token))
     global personal_note_id
@@ -45,6 +29,22 @@ def test_create_personal_note(notes_api, get_token):
     test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
     test2 = notes_api.verify_status_massage(response_body=response.json())
     if test1 + test2 != 0: pytest.fail()
+
+def test_get_home_note(notes_api, get_token):
+    response = notes_api.call_get_notes_api(note_id=home_note_id, headers=notes_api.get_default_header_with_auth(get_token))
+    test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
+    test2 = notes_api.verify_note_title(expected_title=notes_api.home_note_title, response_body=response.json())
+    test3 = notes_api.verify_note_description(expected_description=notes_api.home_note_description, response_body=response.json())
+    test4 = notes_api.verify_note_category(expected_category=notes_api.home_note_category, response_body=response.json())
+    if test1 + test2 + test3 + test4 != 0: pytest.fail()
+
+def test_get_work_note(notes_api, get_token):
+    response = notes_api.call_get_notes_api(note_id=work_note_id, headers=notes_api.get_default_header_with_auth(get_token))
+    test1 = notes_api.verify_status_code_is(expected_status_code=200, actual_status_code=response.status_code)
+    test2 = notes_api.verify_note_title(expected_title=notes_api.work_note_title, response_body=response.json())
+    test3 = notes_api.verify_note_description(expected_description=notes_api.work_note_description, response_body=response.json())
+    test4 = notes_api.verify_note_category(expected_category=notes_api.work_note_category, response_body=response.json())
+    if test1 + test2 + test3 + test4 != 0: pytest.fail()
 
 def test_get_personal_note(notes_api, get_token):
     response = notes_api.call_get_notes_api(note_id=personal_note_id, headers=notes_api.get_default_header_with_auth(get_token))
